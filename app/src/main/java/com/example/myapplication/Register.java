ae35049f2cee650b9891bac7361668b4291158ad
srija9reddy <93567298+srija274@users.noreply.github.com>
2021-11-16 23:25:59 -0500

    added database

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class Register extends AppCompatActivity {
+    FirebaseDatabase rootNode;
+    DatabaseReference reference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        rootNode = FirebaseDatabase.getInstance();
+        reference = rootNode.getReference("users");
+        reference.setValue("");
     }
 }
