0ac695024aa6656418c63f74abf66740db0b8855
Renu <C0814038@mylambton.ca>
2021-11-17 13:45:10 -0800

    added category

--- app/src/main/java/com/example/myapplication/Product.java
+++ app/src/main/java/com/example/myapplication/Product.java
@@ -3,12 +3,16 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Product extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        btn = findViewById(R.id.btn);
     }
 }
