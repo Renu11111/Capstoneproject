4745bccea6c9f17def5ce8553f1e5e82f63725c2
Renu <C0814038@mylambton.ca>
2021-11-17 15:00:40 -0800

    adapter

--- app/src/main/java/Adapter/CategoryAdapter.java
+++ app/src/main/java/Adapter/CategoryAdapter.java
@@ -6,16 +6,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import model.Category;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-
-
 import java.util.List;
+
+import model.Category;
 
 import static android.os.Build.VERSION_CODES.R;
 
