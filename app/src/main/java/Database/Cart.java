ab41de22dc02b9ef21eca34da8f0bb07471b95bb
srija9reddy <93567298+srija274@users.noreply.github.com>
2021-11-24 16:00:18 -0500

    cart

--- app/src/main/java/Database/Cart.java
+++ app/src/main/java/Database/Cart.java
@@ -1,4 +1,25 @@
 package Database;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
 
+@Entity(tableName = "Carts")
 public class Cart {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+    public String emailid;
+    public String name;
+    public double price;
+    public boolean status;
+
+    public Cart( String emailid, String name, double price) {
+        this.emailid = emailid;
+        this.name = name;
+        this.price = price;
+        this.status = false;
+    }
+
+    @Ignore
+    public Cart() {
+    }
 }
--- app/src/main/java/Database/CartDao.java
+++ app/src/main/java/Database/CartDao.java
@@ -1,4 +1,23 @@
 package Database;
 
+import java.util.List;
+
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
 public interface CartDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    public void insert(Cart cart);
+
+    @Query("SELECT * FROM CARTS WHERE emailid=:emailID")
+    public List<Cart> findCart(String emailID);
+
+    @Delete
+    public void deleteOrder(Cart orders);
 }
