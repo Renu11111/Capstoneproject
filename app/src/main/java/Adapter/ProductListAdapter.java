257d0df9829532978f5797270e496d192b444858
srija9reddy <93567298+srija274@users.noreply.github.com>
2021-11-24 17:46:40 -0500

    CartListAdapter and ProductListAdapter

---
+++ app/src/main/java/Adapter/ProductListAdapter.java
@@ -0,0 +1,63 @@
+package Adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ProductListAdapter<Product> extends RecyclerView.Adapter<ProductListAdapter.ProductViewHolder>  {
+    Context context;
+    List<Product> products;
+    private View.OnClickListener clickListener;
+
+    public ProductListAdapter(Context context,List<Product> products,View.OnClickListener clickListener){
+        this.context = context;
+        this.products = products;
+        this.clickListener = clickListener;
+    }
+
+    @NonNull
+    @Override
+    public ProductListAdapter.ProductViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.row_product,parent,false);
+        return new ProductListAdapter.ProductViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProductListAdapter.ProductViewHolder holder, int position) {
+        Product product = products.get(position);
+        holder.txt1.setText(product.name);
+        holder.txt2.setText("$" + product.sale_price);
+        holder.btn.setTag(product);
+        //Glide.with(context).load(product.image_url).into(holder.imageView);
+    }
+
+    @Override
+    public int getItemCount() {
+        return products.size();
+    }
+
+    public class ProductViewHolder extends RecyclerView.ViewHolder{
+
+        TextView txt1,txt2;
+        Button btn;
+        ImageView imageView;
+
+        public ProductViewHolder(@NonNull View itemView) {
+            super(itemView);
+            txt1 = itemView.findViewById(R.id.txt_product_name);
+            txt2 = itemView.findViewById(R.id.txt_product_name);
+            btn = itemView.findViewById(R.id.btn_details);
+            imageView = itemView.findViewById(R.id.image_product);
+            btn.setOnClickListener(clickListener);
+        }
+    }
+}
--- app/src/main/java/Database/CartDao.java
+++ app/src/main/java/Database/CartDao.java
@@ -1,6 +1,6 @@
 package Database;
 
-import java.util.List;
+
 
 import androidx.room.Dao;
 import androidx.room.Delete;
